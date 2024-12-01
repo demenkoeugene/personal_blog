5b8a6aa5d3243134a540fd9ba3c8898d6f0cb1d7
demenkoeugene <mimosa.01-purgers@icloud.com>
2024-12-01 22:28:34 +0200

    BasicAuth added

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 	"personal_blog/model"
 	"personal_blog/utils"
 	"strconv"
 )
 
+func initConfig() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	} else {
+		log.Println("Environment variables loaded successfully")
+	}
+}
 func main() {
+	initConfig()
 	http.HandleFunc("/", getArticleList)
 	http.HandleFunc("/article/{id}", getArticle)
+	http.HandleFunc("/admin", basicAuth(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("This is the protected handler"))
+	},
+	))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
@@ -63,5 +79,40 @@ func getArticle(w http.ResponseWriter, r *http.Request) {
 	if err := utils.RenderArticleTemplate(w, "./templates/articlepage.html", foundArticle); err != nil {
 		http.Error(w, "Failed to render template", http.StatusInternalServerError)
 		log.Println("Error rendering template:", err)
+	}
+}
+
+func basicAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		usernameENV := os.Getenv("ADMIN_USERNAME")
+		passwordENV := os.Getenv("ADMIN_PASSWORD")
+		if usernameENV == "" || passwordENV == "" {
+			log.Fatal("Environment variables ADMIN_USERNAME and ADMIN_PASSWORD must be set")
+		}
+
+		// Extract the username and password from the Authorization header.
+		username, password, ok := r.BasicAuth()
+		if ok {
+			// Hash both provided and expected credentials using SHA-256.
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(usernameENV))
+			expectedPasswordHash := sha256.Sum256([]byte(passwordENV))
+
+			// Use ConstantTimeCompare to compare the hashes.
+			usernameMatch := subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1
+			passwordMatch := subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1
+
+			// If both the username and password match, proceed to the next handler.
+			if usernameMatch && passwordMatch {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		// If authentication fails, return a 401 Unauthorized response with the
+		// WWW-Authenticate header to prompt for basic authentication.
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	}
 }
